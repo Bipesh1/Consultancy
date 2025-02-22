a3ee019147355cdf448f8a0b44afe6d4d3921c83
Bipesh <adbipesh001@gmail.com>
2025-02-22 17:03:07 +0545

    loader added

--- consultancy/src/(protected)/pages/classes.tsx
+++ consultancy/src/(protected)/pages/classes.tsx
@@ -1,21 +1,33 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useTransition } from "react";
 import Classcreate from "../components/class-create";
 import { ClassTable } from "../components/class-table";
 
 export default function ClassAdmin() {
   const [classes, setClasses] = useState<any>([]);
+  const [isPending,startTransition]= useTransition();
   const fetchClasses = async () => {
-    try {
-      const response = await axios.get(`${import.meta.env.VITE_API_URL}/classes`);
-      setClasses(response.data); // Update courses state
-    } catch (err) {
-      console.error("Failed to fetch classes:", err);
-    }
+    startTransition(async()=>{
+      try {
+        const response = await axios.get(`${import.meta.env.VITE_API_URL}/classes`);
+        setClasses(response.data); // Update courses state
+      } catch (err) {
+        console.error("Failed to fetch classes:", err);
+      }
+    })
   };
   useEffect(() => {
     fetchClasses();
   }, []);
+
+  
+  if (isPending) {
+    return (
+      <div className="flex items-center justify-center min-h-screen">
+        <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-blue-900"></div>
+      </div>
+    );
+  }
   return (
     <div className="md:px-5">
        <Classcreate onupdate={fetchClasses} />
