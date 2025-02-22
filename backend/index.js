3e93150869bb95c3a87794a68cdf4f845c8fa81f
Bipesh <adbipesh001@gmail.com>
2025-02-22 15:07:08 +0545

    changes made

--- backend/index.js
+++ backend/index.js
@@ -25,16 +25,6 @@ app.use("/courses", courseRouter);
 app.use("/classes", classRouter);
 app.use("/users", userRouter);
 
-// Serve static files from the client build directory
-const clientBuildPath = path.join(__dirname, "vercel/output");
-app.use(express.static(clientBuildPath));
-console.log("this is " + clientBuildPath)
-console.log("hello")
-
-// Fallback to index.html for client-side routing
-app.get("*", (req, res) => {
-  res.sendFile(path.join(clientBuildPath, "index.html"));
-});
 
 // Connect to the database
 connect();
