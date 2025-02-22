fac65dd3ae9ebd5320d31c40f731439047f6ac72
Bipesh <adbipesh001@gmail.com>
2025-02-22 12:53:33 +0545

    changes made

--- consultancy/eslint.config.js
+++ consultancy/eslint.config.js
@@ -5,25 +5,37 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  { 
+    ignores: ['dist'] 
+  },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+    ],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: {
+        ...globals.browser
+      },
+      parserOptions: {
+        project: './tsconfig.json'
+      }
     },
     plugins: {
+      '@typescript-eslint': tseslint.plugin,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
     rules: {
       '@typescript-eslint/no-unused-vars': 'off',
+      'no-unused-vars': 'off',
       ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
       ],
     },
-  },
+  }
 )
